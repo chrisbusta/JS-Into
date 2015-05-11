7b74e2138ba89a8f421c7cd1a9c06c396164751b
chris bustamante <chrisbusta@hotmsil.com>
2015-05-11 16:21:13 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+var givenName;
+// Q: What does `givenName` equal right now?
+console.log(givenName)
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+var givenName = "Tim"
+console.log(givenName)
+
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+console.log(givenName)//The answer is JD//
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+console.log(greeting)//The answer is Hello, how are you?
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+var high = 50;
+var low = 10;
+console.log(math)//consolelog.log(high-low) equals to 40.
+
+math = high - "5";
+// Q: What is `math` set to?
+var high = 50
+console.log(math-5)// math = high - 5 equals to 40//
+
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+console.log(today-born-1)//console.log(2015-1983-1)is 31//
+
+
+
+// Store some information following in variables.
+var yourName="christian";
+var instructorName="JD";
+
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+
+
+console.log(statement);
